ff317947f3f5d90b38c4fab0284e141a858e8ba8
Paul Parlett <frialey63@googlemail.com>
2022-08-07 19:03:11 +0100

    the SQL files with corrections

---
+++ LAN Times SQL - Offices.sql
@@ -0,0 +1,5 @@
+INSERT INTO offices (office, city, region, mgr, target, sales) VALUES (22,  'Denver'     ,'Western' ,108 ,300000 ,186042);
+INSERT INTO offices (office, city, region, mgr, target, sales) VALUES (11,  'New York'   ,'Eastern' ,106 ,575000 ,692637);
+INSERT INTO offices (office, city, region, mgr, target, sales) VALUES (12,  'Chicago'    ,'Eastern' ,104 ,800000 ,735042);
+INSERT INTO offices (office, city, region, mgr, target, sales) VALUES (13,  'Atlanta'    ,'Eastern' ,105 ,350000 ,367911);
+INSERT INTO offices (office, city, region, mgr, target, sales) VALUES (21,  'Los Angeles','Western' ,108 ,725000 ,835915);
